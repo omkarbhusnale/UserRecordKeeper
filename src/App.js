4c9b4f0ad379e523fd494209f0fe56cb5ffc4f69
Omkar <omkarbhusnale23@gmail.com>
2024-03-03 12:27:11 +0530

    Add API Fetch and Display functionality with help of useEffect hook.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import "./App.css";
 import Header from "./components/Header";
 import { TextField, Button, Stack } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import User from "./components/User";
-// import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
 function App() {
   const [form, setForm] = useState({})
+  const [state, setState] = useState(2)
   const [userList, setUserList] = useState([])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idstarts=1001`)
+      const res = await get.json()
+
+      setData(res)
+      console.log(res)
+    }
+
+    getData()
+  }, [state])
 
   const addUser = () => {
     if (!(form.name === "" || form.email === "")) {
@@ -22,11 +36,18 @@ function App() {
     }
   }
 
-  // const deleteUser = (index) => {
-  //   let tempArr = userList
-  //   tempArr.splice(index, 1);
-  //   setUserList([...tempArr])
-  // }
+  const handleDown = (e) => {
+    if (e.key === "Enter") {
+      addUser()
+    }
+  }
+
+  const deleteUser = (index) => {
+    let tempArr = data
+    tempArr.splice(index, 1);
+    setData([...tempArr])
+    setState(tempArr.length)
+  }
 
   return (
     <div className="App">
@@ -35,20 +56,32 @@ function App() {
         <Stack direction="row" spacing={2}>
           <TextField
             value={form.name}
+            onKeyDown={(e) => handleDown(e)}
             onChange={(e) => setForm({ ...form, name: e.target.value })}
             id="outlined-basic"
             label="Name" variant="outlined" />
           <TextField
             value={form.email}
+            onKeyDown={(e) => handleDown(e)}
             onChange={(e) => setForm({ ...form, email: e.target.value })}
             id="outlined-basic"
             label="Email" variant="outlined"
           />
           <Button variant="contained" onClick={addUser}>
+            <AddIcon /> Add Local Data
+          </Button>
+
+          <Button
+            onClick={() => setState(state + 2)}
+            variant="contained"
+            color="error">
             <AddIcon />
+            Fetch API Data ({state})
           </Button>
         </Stack>
       </div>
+
+
 
       <div className="userList">
         <div className="user">
@@ -56,6 +89,22 @@ function App() {
           <h4>Email</h4>
           <h4>Remove</h4>
         </div>
+        {
+          data.map((ele, idx) => {
+            return (
+              <div className="user" key={idx}>
+                <h4>{ele.firstName} {ele.lastName}</h4>
+                <h4>{ele.email}</h4>
+                <Button
+                  onClick={() => deleteUser(idx)}
+                  variant="contained"
+                  color="error">
+                  <DeleteIcon />
+                </Button>
+              </div>
+            )
+          })
+        }
         {
           userList.map((e, index) => {
             return (
@@ -68,16 +117,6 @@ function App() {
                   userList={userList}
                   setUserList={setUserList}
                 />
-                {/* <div className="user" key={index}>
-                  <h4>{e.name}</h4>
-                  <h4>{e.email}</h4>
-                  <Button
-                    onClick={() => deleteUser(index)}
-                    variant="contained"
-                    color="error">
-                    <DeleteIcon />
-                  </Button>
-                </div> */}
               </>
             )
           })
